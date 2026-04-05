3ba0faef8c081cc15507dd7c1ee391e79b0204e6
Alan Kydd <kydd.alan.g@gmail.com>
2026-04-04 21:53:47 -0600

    Allow ecs_task_execution to inject secrets into container on startup

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -111,6 +111,23 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy" "ecs_task_execution_secrets" {
+  name = "secrets-read"
+  role = aws_iam_role.ecs_task_execution.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["secretsmanager:GetSecretValue"]
+      Resource = [
+        aws_secretsmanager_secret.db_password.arn,
+        aws_secretsmanager_secret.jwt_secret.arn
+      ]
+    }]
+  })
+}
+
 resource "aws_iam_role" "ecs_task" {
   name = "ecs-task-role"
 
